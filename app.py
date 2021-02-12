2b9d339f6cdf2a2cbb01d5b172731aa93f39c6b9
taisuke-suzuki <summer_wars_5016@ezweb.nejp>
2021-02-12 19:07:38 +0900

    improve

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 
+@app.route("/")
+def index():
+    return "hello world!"
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
